6fb2e2144918b56e3c772c9ffd0a9625c3901f8b
metalmilitia069 <medina.engineering@outlook.com>
2021-05-19 21:16:49 -0400

    Implementing Input System 2.0

--- Assets/_Final Assets/Scripts/PlayerControls.cs
+++ Assets/_Final Assets/Scripts/PlayerControls.cs
@@ -19,9 +19,17 @@ public class @PlayerControls : IInputActionCollection, IDisposable
             ""id"": ""df12c6cb-f8f5-4c92-9f33-82a5bd36b704"",
             ""actions"": [
                 {
-                    ""name"": ""Move"",
+                    ""name"": ""SideMove"",
                     ""type"": ""PassThrough"",
                     ""id"": ""71633047-4dbd-4303-8b74-ee5368f58398"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ForwardMove"",
+                    ""type"": ""Button"",
+                    ""id"": ""9ad8dcb7-3fc6-4610-acde-3428da567e60"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
@@ -29,13 +37,13 @@ public class @PlayerControls : IInputActionCollection, IDisposable
             ],
             ""bindings"": [
                 {
-                    ""name"": ""Side"",
+                    ""name"": ""Keyboard"",
                     ""id"": ""9958975b-6d8d-4e03-881d-6d8b8398246a"",
                     ""path"": ""1DAxis"",
                     ""interactions"": """",
                     ""processors"": ""AxisDeadzone"",
                     ""groups"": """",
-                    ""action"": ""Move"",
+                    ""action"": ""SideMove"",
                     ""isComposite"": true,
                     ""isPartOfComposite"": false
                 },
@@ -46,7 +54,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""Move"",
+                    ""action"": ""SideMove"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
                 },
@@ -57,7 +65,106 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""Move"",
+                    ""action"": ""SideMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Gamepad"",
+                    ""id"": ""73be3a04-e1df-40de-9de8-82d609acac1f"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SideMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""d27158e5-a81c-488b-98a8-cfec6b736369"",
+                    ""path"": ""<XInputController>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SideMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""0a8d6ccf-b8f0-45ce-a106-f38d7e4ec05b"",
+                    ""path"": ""<XInputController>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SideMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""d4b93b4a-7884-4d14-8e07-690fba6b608f"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ffbb41be-5276-4b5f-9c5e-9f212887f686"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""dfd910e0-afa2-4f8d-b5bf-df322568ec36"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Gamepad"",
+                    ""id"": ""7c37e248-cd64-40c9-804b-2fe4b98d1f2d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""7645212a-ed73-4113-b30a-5ab6c4e1f4e6"",
+                    ""path"": ""<XInputController>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""be21ff68-6a28-4619-bc6f-0d5129bfd4c0"",
+                    ""path"": ""<XInputController>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForwardMove"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
                 }
@@ -95,7 +202,8 @@ public class @PlayerControls : IInputActionCollection, IDisposable
 }");
         // LocomotionTopView
         m_LocomotionTopView = asset.FindActionMap("LocomotionTopView", throwIfNotFound: true);
-        m_LocomotionTopView_Move = m_LocomotionTopView.FindAction("Move", throwIfNotFound: true);
+        m_LocomotionTopView_SideMove = m_LocomotionTopView.FindAction("SideMove", throwIfNotFound: true);
+        m_LocomotionTopView_ForwardMove = m_LocomotionTopView.FindAction("ForwardMove", throwIfNotFound: true);
         // LocomotionSideView
         m_LocomotionSideView = asset.FindActionMap("LocomotionSideView", throwIfNotFound: true);
         m_LocomotionSideView_Newaction = m_LocomotionSideView.FindAction("New action", throwIfNotFound: true);
@@ -148,12 +256,14 @@ public class @PlayerControls : IInputActionCollection, IDisposable
     // LocomotionTopView
     private readonly InputActionMap m_LocomotionTopView;
     private ILocomotionTopViewActions m_LocomotionTopViewActionsCallbackInterface;
-    private readonly InputAction m_LocomotionTopView_Move;
+    private readonly InputAction m_LocomotionTopView_SideMove;
+    private readonly InputAction m_LocomotionTopView_ForwardMove;
     public struct LocomotionTopViewActions
     {
         private @PlayerControls m_Wrapper;
         public LocomotionTopViewActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Move => m_Wrapper.m_LocomotionTopView_Move;
+        public InputAction @SideMove => m_Wrapper.m_LocomotionTopView_SideMove;
+        public InputAction @ForwardMove => m_Wrapper.m_LocomotionTopView_ForwardMove;
         public InputActionMap Get() { return m_Wrapper.m_LocomotionTopView; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -163,16 +273,22 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_LocomotionTopViewActionsCallbackInterface != null)
             {
-                @Move.started -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnMove;
-                @Move.performed -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnMove;
-                @Move.canceled -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnMove;
+                @SideMove.started -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnSideMove;
+                @SideMove.performed -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnSideMove;
+                @SideMove.canceled -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnSideMove;
+                @ForwardMove.started -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnForwardMove;
+                @ForwardMove.performed -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnForwardMove;
+                @ForwardMove.canceled -= m_Wrapper.m_LocomotionTopViewActionsCallbackInterface.OnForwardMove;
             }
             m_Wrapper.m_LocomotionTopViewActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @Move.started += instance.OnMove;
-                @Move.performed += instance.OnMove;
-                @Move.canceled += instance.OnMove;
+                @SideMove.started += instance.OnSideMove;
+                @SideMove.performed += instance.OnSideMove;
+                @SideMove.canceled += instance.OnSideMove;
+                @ForwardMove.started += instance.OnForwardMove;
+                @ForwardMove.performed += instance.OnForwardMove;
+                @ForwardMove.canceled += instance.OnForwardMove;
             }
         }
     }
@@ -212,7 +328,8 @@ public class @PlayerControls : IInputActionCollection, IDisposable
     public LocomotionSideViewActions @LocomotionSideView => new LocomotionSideViewActions(this);
     public interface ILocomotionTopViewActions
     {
-        void OnMove(InputAction.CallbackContext context);
+        void OnSideMove(InputAction.CallbackContext context);
+        void OnForwardMove(InputAction.CallbackContext context);
     }
     public interface ILocomotionSideViewActions
     {
--- Assets/_Final Assets/Scripts/ShipBase.cs
+++ Assets/_Final Assets/Scripts/ShipBase.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class ShipBase : MonoBehaviour
 {
+
+    private float _movementInput;
+    [SerializeField]
+    private float _speed;
+    private PlayerControls _playerControls;
+
+    private float _playerMovementX;
+    private float _playerMovementZ;
+    private Vector3 _playerMovement = new Vector3();
+
+    private Vector3 playerPos;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        _movementInput = _playerControls.LocomotionTopView.SideMove.ReadValue<float>();
+
+        _playerMovementX = _playerControls.LocomotionTopView.SideMove.ReadValue<float>() * _speed * Time.deltaTime;
+        _playerMovementZ = _playerControls.LocomotionTopView.ForwardMove.ReadValue<float>() * _speed * Time.deltaTime;
+
+        _playerMovement = new Vector3(_playerMovementX, 0, _playerMovementZ);
+
+
+
+        this.transform.position += _playerMovement;
+
+        //playerPos = this.transform.position;
+
+        //playerPos.x += (_movementInput * _speed * Time.deltaTime);
+
+        //this.transform.position = playerPos;
+
+
     }
 }
