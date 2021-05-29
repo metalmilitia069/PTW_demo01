08d883820e066c5476c7dda08a4844ac92c61336
metalmilitia069 <medina.engineering@outlook.com>
2021-05-28 22:48:58 -0400

    Spawn Manager Rework

--- Assets/_Final Assets/Scripts/SpawnManager.cs
+++ Assets/_Final Assets/Scripts/SpawnManager.cs
@@ -26,18 +26,8 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
-    public void SpawnEnemies()
+    public IEnumerator SpawnEnemies()
     {
-        if (_spawnIndex > spawnManager_so.waveConfig[_waveIndex].waveEnemyTypes.Length -1)
-        {
-            _spawnIndex = 0;
-            _waveIndex++;
-            if (_waveIndex > spawnManager_so.waveConfig.Length -1) //or its the spawn process is paused TODO
-            {
-                return;
-            }            
-        }
-
         for (int i = 0; i < spawnManager_so.waveConfig[_waveIndex].numberOfEnemiesByType[_spawnIndex]; i++)
         {
             spawnManager_so._currentEnemiesList.Add(Instantiate(spawnManager_so.waveConfig[_waveIndex].waveEnemyTypes[_spawnIndex], new Vector3(0, 0, 0), Quaternion.identity));
@@ -45,15 +35,74 @@ public class SpawnManager : MonoBehaviour
         }
 
         _spawnIndex++;
+
+        if (_spawnIndex > spawnManager_so.waveConfig[_waveIndex].waveEnemyTypes.Length - 1)
+        {
+
+            _spawnIndex = 0;
+
+            _waveIndex++;
+
+            if (_waveIndex > spawnManager_so.waveConfig.Length - 1) //or its the spawn process is paused TODO
+            {
+                StopCoroutine(SpawnEnemies());
+            }
+            else
+            {
+                yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeToTheNextWave);
+            }
+        }
+        else
+        {
+            //Instead of setting the pre-determined time to start spawnning the next type of enemies, the next set should be spawned after all 
+            //enemies from the last spawn being destroyed
+            yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeBetweenSpawns);
+        }
     }
 
-    public IEnumerable TimeBetweenSpawns()
+    public IEnumerator SpawnEnemies2()
+    {
+        for (int i = 0; i < spawnManager_so.waveConfig[_waveIndex].numberOfEnemiesByType[_spawnIndex]; i++)
+        {
+            spawnManager_so._currentEnemiesList.Add(Instantiate(spawnManager_so.waveConfig[_waveIndex].waveEnemyTypes[_spawnIndex], new Vector3(0, 0, 0), Quaternion.identity));
+
+        }
+
+        _spawnIndex++;
+
+        if (_spawnIndex > spawnManager_so.waveConfig[_waveIndex].waveEnemyTypes.Length - 1)
+        {
+
+            _spawnIndex = 0;
+
+            _waveIndex++;
+
+            if (_waveIndex > spawnManager_so.waveConfig.Length - 1) //or its the spawn process is paused TODO
+            {
+                StopCoroutine(SpawnEnemies2());
+            }
+            else
+            {
+                yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeToTheNextWave);
+            }
+        }
+        //else
+        //{
+        //    //Instead of setting the pre-determined time to start spawnning the next type of enemies, the next set should be spawned after all 
+        //    //enemies from the last spawn being destroyed
+        //    yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeBetweenSpawns);
+        //}
+    }
+
+
+
+    public IEnumerator TimeBetweenSpawns()
     {
         yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeBetweenSpawns);
         _spawnIndex++; 
     }
 
-    public IEnumerable TimeToTheNextWave()
+    public IEnumerator TimeToTheNextWave()
     {
         yield return new WaitForSeconds(spawnManager_so.waveConfig[_waveIndex].timeToTheNextWave);
     }
