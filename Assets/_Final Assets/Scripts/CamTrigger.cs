e8e29e92e9bb26b6d300a7b6e85d5768797bacf8
metalmilitia069 <medina.engineering@outlook.com>
2021-05-31 03:09:38 -0400

    Implement Cam Triggers

--- Assets/_Final Assets/Scripts/CamTrigger.cs
+++ Assets/_Final Assets/Scripts/CamTrigger.cs
@@ -8,6 +8,10 @@ public class CamTrigger : MonoBehaviour
     private Vector3 _originCoordinates;
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private bool unlock = false;
+
+    private ShipBase shipBase;
 
     // Start is called before the first frame update
     void Start()
@@ -18,36 +22,95 @@ public class CamTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (unlock)
+        {
+            float componentX;
+            float componentY;
+            float componentZ;
+            if (Vector3.Distance(shipBase.transform.position, _originCoordinates) > 1f)
+            {
+                if (shipBase.transform.position.x - _originCoordinates.x > 0)
+                {
+                    componentX = -1 * _speed * Time.deltaTime;
+                }
+                else
+                {
+                    componentX = _speed * Time.deltaTime;
+                }
+
+                if (shipBase.transform.position.y - _originCoordinates.y > 0)
+                {
+                    componentY = -1 * _speed * Time.deltaTime;
+                }
+                else
+                {
+                    componentY = _speed * Time.deltaTime;
+                }
+
+                if (shipBase.transform.position.z - _originCoordinates.z > 0)
+                {
+                    componentZ = -1 * _speed * Time.deltaTime;
+                }
+                else
+                {
+                    componentZ = _speed * Time.deltaTime;
+                }
+
+                shipBase.transform.position += new Vector3(componentX, componentY, componentZ);
+            }
+            else
+            {
+                unlock = false;
+            } 
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<ShipBase>())
         {
-            other.GetComponent<ShipBase>().controllerManager_SO.controlSwitcher = 2;
+            
+            unlock = true;
+            shipBase = other.GetComponent<ShipBase>();
 
-            float componentX;
-            float componentY;
-            float componentZ;
+            //other.GetComponent<ShipBase>().controllerManager_SO.controlSwitcher = 2;
 
-            while (other.transform.position != _originCoordinates)
-            {
-                if (other.transform.position.x - _originCoordinates.x > 0)
-                {
+            //float componentX;
+            //float componentY;
+            //float componentZ;
 
-                }
 
-                if (other.transform.position.y - _originCoordinates.y > 0)
-                {
+            //while (Vector3.Distance(other.transform.position, _originCoordinates) > 1f)
+            //{
+            //    if (other.transform.position.x - _originCoordinates.x > 0)
+            //    {
+            //        componentX = -1 * _speed * Time.deltaTime;
+            //    }
+            //    else
+            //    {
+            //        componentX = _speed * Time.deltaTime;
+            //    }
 
-                }
+            //    if (other.transform.position.y - _originCoordinates.y > 0)
+            //    {
+            //        componentY = -1 * _speed * Time.deltaTime;
+            //    }
+            //    else
+            //    {
+            //        componentY = _speed * Time.deltaTime;
+            //    }
 
-                if (other.transform.position.z - _originCoordinates.z > 0)
-                {
+            //    if (other.transform.position.z - _originCoordinates.z > 0)
+            //    {
+            //        componentZ = -1 * _speed * Time.deltaTime;
+            //    }
+            //    else
+            //    {
+            //        componentZ = _speed * Time.deltaTime;
+            //    }
 
-                }
-            }
+            //    other.transform.position += new Vector3(componentX, componentY, componentZ);
+            //}
         }
     }
 }
