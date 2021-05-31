b1905d7a57e6e772bf51bdf6af78dc1d7b8bd276
metalmilitia069 <medina.engineering@outlook.com>
2021-05-31 01:26:04 -0400

    Cam Change Trigger Implementation 01

--- Assets/_Final Assets/Scripts/CameraTransitions.cs
+++ Assets/_Final Assets/Scripts/CameraTransitions.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class CameraTransitions : MonoBehaviour
 {
-    public CameraTransitionsPool_SO cameraTransitionsPool_SO;
+    //public CameraTransitionsPool_SO cameraTransitionsPool_SO;
+    public ControllerManager_SO controllerManager_SO;
 
     public GameObject[] camTransitionsArray = new GameObject[1];
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraTransitionsPool_SO.camTransitionsArray = camTransitionsArray;
+        //cameraTransitionsPool_SO.camTransitionsArray = camTransitionsArray;
+
     }
 
     // Update is called once per frame
@@ -28,6 +30,22 @@ public class CameraTransitions : MonoBehaviour
             {
                 item.gameObject.SetActive(false);
             }
+            
         }
+    }
+
+    public void TopControlOn()
+    {
+        controllerManager_SO.controlSwitcher = -1;
+    }
+
+    public void SideControlOn()
+    {
+        controllerManager_SO.controlSwitcher = 0;
+    }
+
+    public void BackControlOn()
+    {
+        controllerManager_SO.controlSwitcher = 1;
     }
 }
