095b8cf26f93195d15b3e01fd0c80cc1f57d20b2
metalmilitia069 <medina.engineering@outlook.com>
2021-06-10 23:42:23 -0400

    Enemy AI Implementation 01

---
+++ Assets/_Final Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : EnemyBase
+{
+    [Header("Enemy Stats")]
+    [SerializeField]
+    protected int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
