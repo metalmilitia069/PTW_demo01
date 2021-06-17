b17e472e9f052a6ab7fd085c95e999aece5bd9c3
metalmilitia069 <medina.engineering@outlook.com>
2021-06-17 17:52:53 -0400

    Enemy AI Implementation 11

--- Assets/_Final Assets/Scripts/EnemyBehaviorShieldedTurret.cs
+++ Assets/_Final Assets/Scripts/EnemyBehaviorShieldedTurret.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviorShieldedTurret : MonoBehaviour
+public class EnemyBehaviorShieldedTurret : EnemyCombat
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
