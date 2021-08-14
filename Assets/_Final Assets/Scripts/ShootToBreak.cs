abc2787c0c01c734067eed1158fd05f4850cbb44
Cousin Itt <medina.engineering@outlook.com>
2021-08-14 10:17:30 -0400

    Assets Environment Breakable Door

---
+++ Assets/_Final Assets/Scripts/ShootToBreak.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootToBreak : MonoBehaviour
+{
+    public GameObject wholeItem;
+    public GameObject brokenItem;
+
+    public int itemHP = 10;
+    public float timeToDestroy = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<ProjectileBase>())
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        itemHP--;
+
+        if (itemHP <= 0)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        wholeItem.gameObject.SetActive(false);
+        brokenItem.gameObject.SetActive(true);
+
+        Destroy(this.gameObject, timeToDestroy);
+    }
+}
