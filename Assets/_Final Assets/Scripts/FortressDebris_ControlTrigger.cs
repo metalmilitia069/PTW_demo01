4ec1e07d0672b55aa83bef2666fd3252ceeb8af2
Cousin Itt <medina.engineering@outlook.com>
2021-08-30 02:51:14 -0400

    Fixing Sea Level 01

--- Assets/_Final Assets/Scripts/FortressDebris_ControlTrigger.cs
+++ Assets/_Final Assets/Scripts/FortressDebris_ControlTrigger.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FortressDebris_ControlTrigger : MonoBehaviour
 {
     public FortressDebris_SO fortressDebris_SO;
+    public SeaDebrisEndWall_SO seaDebrisEndWall_SO;
 
     // Start is called before the first frame update
     void Start()
     {
         fortressDebris_SO.canMove = true;
+        seaDebrisEndWall_SO.isKillingSeaDebris = true;
         Destroy(this.gameObject, 5.0f);
     }    
 }
